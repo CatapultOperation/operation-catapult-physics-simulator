a62885f86b9a6ba755747cce140fe3ff682847dc
Jack DeLano <jsdsparky@gmail.com>
2016-07-18 15:26:32 -0400

    particle and staticfield classes made
    I may have accidentally committed an xml file that my ide generated in a
    previous commit.

--- src/main/python/physicssim/physics/staticfield.py
+++ src/main/python/physicssim/physics/staticfield.py
@@ -1,1 +1,24 @@
-class StaticField
+class StaticField:
+	def __init__(self, topLeft, bottomRight, strength, direction):
+		"""Takes arguments topLeft as tuple (x, y), bottomRight as tuple (x, y), strength (in N/C),
+		and direction in radians"""
+		self.topLeft = topLeft
+		self.bottomRight = bottomRight
+		self.strength = strength
+		self.direction = direction
+
+	def getTopLeft(self):
+		"""returns top left coordinate of field as tuple (x, y)"""
+		return self.topLeft
+
+	def getBottomRight(self):
+		"""returns bottom right coordinate of field as tuple (x, y)"""
+		return self.bottomRight
+
+	def getStrength(self):
+		"""returns strength of field (in N/C)"""
+		return self.strength
+
+	def getDirection(self):
+		"""returns direction of field in radians"""
+		return self.direction
