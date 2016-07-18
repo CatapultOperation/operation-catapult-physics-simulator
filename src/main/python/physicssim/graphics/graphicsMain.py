b29619de44599a4281c6ff44d200fb0eeae1b53d
hjoyn <hjoyn1011@gmail.com>
2016-07-18 10:48:24 -0400

    Finished render circles and click and drag move circles

--- src/main/python/physicssim/graphics/graphicsMain.py
+++ src/main/python/physicssim/graphics/graphicsMain.py
@@ -6,5 +6,13 @@ from pygame.locals import *
 import mouseEvents as mouse
 
 
+def render(screen, objects):
+    screen.fill((0, 0, 0))
+    
+    for object in objects: 
+        pygame.draw.circle(screen, object.color, object.center, object.radius)
+    
+    pygame.display.flip()
+
 def events(screen, objects, events):
     mouse.events(screen, objects, events)
