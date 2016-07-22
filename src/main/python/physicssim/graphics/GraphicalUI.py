fc6ee957beae666ae0cc2a809c6d0fd70f3e4247
hjoyn <hjoyn1011@gmail.com>
2016-07-22 10:03:35 -0400

    Initial UI class commit

---
+++ src/main/python/physicssim/graphics/GraphicalUI.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class GraphicalUI: 
+    
+    
+    def draw(self, screen):
+        UI = pygame.Surface((300, 300))
+        
+    
+    def mouseCollision(self, event):
+        
+    
+    def 
