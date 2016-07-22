0a9fae98a3213a92d3200c171cde6900fccee7c1
Jack DeLano <jsdsparky@gmail.com>
2016-07-22 09:59:59 -0400

    Made ui images and access to them

--- src/main/python/physicssim/menuItems/draggableParticle.py
+++ src/main/python/physicssim/menuItems/draggableParticle.py
@@ -1,13 +1,16 @@
 import pygame
 import os
 from main.python.physicssim.physics.particle import Particle
+from main.python.physicssim.graphics.GraphicalParticle import Charge
+from main.python.physicssim.menuItems.Size import Size
+from main.python.physicssim.menuItems.Strength import Strength
 
 class DraggableParticle:
-	def __init__(self, pos, mass, charge, timeInterval):
+	def __init__(self, pos, charge):
+		"""Pass pos as list [x, y], pass charge as Charge enum"""
+		self.moveState = False
 		self.pos = pos
-		self.mass = mass
 		self.charge = charge
-		self.timeInterval = timeInterval
 		thisdir = os.path.dirname(__file__)
 		self.dir = os.path.dirname(os.path.dirname(os.path.dirname((os.path.dirname(thisdir)))))
 
@@ -20,6 +23,24 @@ class DraggableParticle:
 		pic.set_colorkey((255, 255, 255))
 		screen.blit(pic, self.pos)
 
-	def getObject(self):
-		"""Returns object to add to list for drawing"""
-		return Particle(self.pos, self.mass, self.charge, self.timeInterval)
+	def getObject(self, size, strength, timeInterval):
+		"""Returns object to add to list for drawing;
+		size as Size enum, strength as Strength enum, timeInterval is time between updates"""
+		if size == Size.SMALL:
+			mass = 1
+		elif size == Size.MED:
+			mass = 2
+		else:
+			mass = 3
+
+		if strength == Strength.LOW:
+			charge = .01
+		elif strength == Strength.MED:
+			charge = .02
+		else:
+			charge = .03
+
+		if self.charge == Charge.NEGATIVE:
+			charge *= -1
+
+		return Particle(self.pos, mass, self.charge, timeInterval)
