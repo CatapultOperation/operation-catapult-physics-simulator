ebf39149ef693b7cedaba48891b4f1bbb7a9b78c
hjoyn <hjoyn1011@gmail.com>
2016-07-22 10:14:10 -0400

    Quick GraphicalUI edi

--- src/main/python/physicssim/graphics/GraphicalUI.py
+++ src/main/python/physicssim/graphics/GraphicalUI.py
@@ -4,11 +4,18 @@ import pygame
 class GraphicalUI: 
     
     
+    
+    def __init__(self):
+        self.buttonRectList = []
+    
     def draw(self, screen):
         UI = pygame.Surface((300, 300))
         
+        
     
     def mouseCollision(self, event):
+        for buttonRect in self.buttonRectList: 
+            
         
     
     def 
